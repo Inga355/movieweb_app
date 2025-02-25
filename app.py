9a953b5311bf2b35b76991d26e56120db7eee46a
Inga355 <albi1303@gmail.com>
2025-02-25 11:30:58 +0100

    added functionality for using extern API, updated home route

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, render_template
 from data_management.sqlite_data_manager import SQLiteDataManager
+import requests
 
 app = Flask(__name__)
-data_manager = SQLiteDataManager('data/movieapp.db')
+db_manager = SQLiteDataManager('data/movieapp.db')
+OMDB_API_KEY = "api_key"
+OMDB_API_URL = "http://www.omdbapi.com/?apikey={}&t={}"
 
 
 @app.route('/')
 def home():
-    return "Welcome to MovieWeb App!"
+    users = db_manager.get_all_users()
+    return render_template('users.html', users=users)
 
 
 @app.route('/users')
 def users():
-    users = data_manager.get_all_users()
+    users = db_manager.get_all_users()
     return render_template('users.html')
+
+
+
 
 
 if __name__ == '__main__':
