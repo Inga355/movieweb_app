ea41f0ca42756e53120b56edc3a255361342c1a7
Inga355 <albi1303@gmail.com>
2025-02-25 11:40:45 +0100

    added route add_user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from data_management.sqlite_data_manager import SQLiteDataManager
 import requests
 
@@ -21,7 +21,13 @@ def user_movies(user_id):
     return render_template('user_movies.html', user=user, movies=movies)
 
 
-
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        name = request.form['name']
+        db_manager.add_user(name)
+        return redirect(url_for('home'))
+    return render_template('add_user.html')
 
 
 
