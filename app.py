afdfdcc4f22506eb98d7f22c4150e5770ab3ced0
Inga355 <albi1303@gmail.com>
2025-02-25 11:08:38 +0100

    added user_movies.html template

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from data_management.sqlite_data_manager import SQLiteDataManager
 
 app = Flask(__name__)
-data_manager = SQLiteDataManager("data/movieapp.db")
+data_manager = SQLiteDataManager('data/movieapp.db')
+
 
 @app.route('/')
 def home():
     return "Welcome to MovieWeb App!"
 
+
 @app.route('/users')
-def list_users():
+def users():
     users = data_manager.get_all_users()
-    return str(users)
+    return render_template('users.html')
 
 
 if __name__ == '__main__':
